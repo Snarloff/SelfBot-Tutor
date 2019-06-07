da60125b04cce54620b727477c30b012fe420b40
Punisher Fusion <pnsgraal@gmail.com>
2019-06-06 22:10:12 -0300

    [03] Commit.
    Arquivo main.py atualizado.

---
+++ main.py
@@ -0,0 +1,7 @@
+#Após ter baixado o dsicrod.py vamos importá-lo.
+#Para isso vamos utilizar o "import".
+#Vamos também importar o "json" para carregar o arquivo de config do bot.
+
+import discord
+import json
+from discord.ext.commands import Bot
